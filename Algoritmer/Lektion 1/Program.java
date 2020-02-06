11ec41799b89794751229eea0f61194fb9a1dacb
Christian Pone <ChristiaNotarmaso@gmail.com>
2020-02-06 14:21:55 +0100

    ændringer

--- Algoritmer/Lektion 1/Program.java
+++ Algoritmer/Lektion 1/Program.java
@@ -1,11 +1,11 @@
 public class Program {
 
 
-    int f(int[] A, int n) {
+    int recursion(int[] A, int n) {
         if (n == 0) {
             return 0;
         } else
-            return f(A, n - 1) + A[n - 1];
+            return recursion(A, n - 1) + A[n - 1];
     }
 
     int iterativ(int n) {
@@ -22,4 +22,50 @@ public class Program {
 
     }
 
+    public static void main(String[] args) {
+        int[] tal = {2,1,3,2,3,11,1,5,7,10};
+        it(tal, 0,9);
+
+    }
+
+    static int it(int A[], int i, int j) {
+
+        boolean max = true;
+
+
+
+        //int m = ((i + j) / 2);
+        int m = 4;
+
+        while (max) {
+
+
+            if (A[m] >= A[m - 1] && A[m] >= A[m + 1]) {
+
+
+                max = false;
+
+
+            } else if (A[m - 1] > A[m]) {
+                System.out.println("hej");
+                m = ((i + A[m-1])/2);
+                continue;
+
+            } else if(A[m] < A[m+1]){
+                System.out.println("hej");
+                m=(A[m+1]+j/2);
+                continue;
+
+            }
+
+
+        }
+        System.out.println(m);
+        System.out.println(A[m]);
+        return m;
+    }
+
 }
+
+
+
